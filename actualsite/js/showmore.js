d8832ff8e83b8a3b03e1a5383524cae1cfd85bac
itsnathanlam <nathan.lam@outlook.com>
2016-06-30 00:36:42 -0700

    removed extra ";"

--- actualsite/js/showmore.js
+++ actualsite/js/showmore.js
@@ -3,7 +3,7 @@ $(document).ready( function() {
 var button = document.getElementById('showmore-button');
 
 button.onclick = function() {
-    var div = document.getElementById('product-showmore').className = "product-showmore";;
+    var div = document.getElementById('product-showmore').className = "product-showmore";
     if (div.style.display !== 'none') {
         div.style.display = 'none';
     }
