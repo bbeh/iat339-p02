ad8c450d2aa779e4f218bc6c77b3da4a9d15e3df
bbeh <bbeh@sfu.ca>
2016-06-30 12:01:18 -0700

    Updated Javascript Library and Fixed Show More Button

--- actualsite/js/showmore.js
+++ actualsite/js/showmore.js
@@ -1,15 +1,27 @@
-$(document).ready( function() {
+$(document).ready( 
 
-var button = document.getElementById('showmore-button');
+	function() {
 
-button.onclick = function() {
-    var showmore = document.getElementById('product-showmore');
-    if (showmore.style.display !== 'none') {
-        showmore.style.display = 'none';
-    }
-    else {
-        showmore.style.display = 'block';
-    }
-};
+		$(".showmore-button").click(
 
-});
+			function(clickEvent) {
+
+				console.log ("clicked");
+
+			    var showmore = document.getElementById('product-showmore');
+			    var showmorebtn = document.getElementById('showmore-button');
+
+			    if ( $("#product-showmore").is(":visible")) {
+			    	//is visible do stuff
+			    } else {
+			    	$("#showmore-button").hide();	
+			    	$("#product-showmore").fadeIn(1500);
+			    }
+
+			}
+
+	
+		);
+
+	}
+	);
